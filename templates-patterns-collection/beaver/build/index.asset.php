b0ad0b42a36808b5835d6c91bdfe546cb8bf1bb0
Sean Murphy <sean@iamseanmurphy.com>
2024-11-28 16:18:24 +0000

    1.2.14

--- templates-patterns-collection/beaver/build/index.asset.php
+++ templates-patterns-collection/beaver/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => 'ca8f712a026449ad2952');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '1b33958ecd0f5e73800b');
