6f5670d6ab578772c81b142cb0070bf0136e61d5
Sean Murphy <sean@iamseanmurphy.com>
2026-03-31 08:36:12 +0000

    1.2.26

--- templates-patterns-collection/vendor/composer/installed.php
+++ templates-patterns-collection/vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'codeinwp/templates-patterns-collection',
-        'pretty_version' => '1.2.25',
-        'version' => '1.2.25.0',
+        'pretty_version' => '1.2.26',
+        'version' => '1.2.26.0',
         'reference' => null,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
@@ -11,8 +11,8 @@
     ),
     'versions' => array(
         'codeinwp/templates-patterns-collection' => array(
-            'pretty_version' => '1.2.25',
-            'version' => '1.2.25.0',
+            'pretty_version' => '1.2.26',
+            'version' => '1.2.26.0',
             'reference' => null,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
